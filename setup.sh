f5b90dae492596083a4a099cc005e65d39d0b3ae
mikedelcastillo <johnmichaeldc@gmail.com>
2021-07-24 19:26:44 +0800

    Added clears in setup.sh for readability

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-echo "Checking permissions"
+clear && echo "Checking permissions"
 is_user_root () { [ "${EUID:-$(id -u)}" -eq 0 ]; }
 if is_user_root; then
     echo "Running as root"
@@ -7,24 +7,24 @@ else
     exit 1
 fi
 
-echo "Updating system"
+clear && echo "Updating system"
 apt-get update  -y --fix-missing
 apt-get upgrade -y
 apt-get autoremove -y
 
-echo "Install linux dependencies"
+clear && echo "Install linux dependencies"
 apt-get install -y git --fix-missing
 
-echo "Install node"
+clear && echo "Install node"
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
 bash n lts
 rm n
 
-echo "Install node dependencies"
+clear && echo "Install node dependencies"
 npm install -g pm2 nodemon
 npm install
 
-echo "Setting up mjpeg-streamer"
+clear && echo "Setting up mjpeg-streamer"
 raspi-config nonint do_camera 1
 apt-get install -y cmake libjpeg8-dev gcc g++
 rm -rf mjpg-streamer
@@ -34,7 +34,7 @@ make
 make install
 cd ../../
 
-echo "Setting up pm2"
+clear && echo "Setting up pm2"
 PM2_USER="pi"
 pm2 del all
 pm2 start "npm run camera-hd" --name "camera"
